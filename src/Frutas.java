1ac62a4317cc0f93d49f88b71131c0fb11cf7795
Shoham <medinashoham@gmail.com>
2023-02-17 07:38:11 -0600

    Initial commit

---
+++ src/Frutas.java
@@ -0,0 +1,11 @@
+
+    public  class fruta {
+        private String type; // Platano, mango, pepino, coco
+        private String flover; // agridulce, dulce
+        private String quality; // Madura, verde
+        private  double price;  // 25, 12,18
+    }
+}
+
+
+    }
